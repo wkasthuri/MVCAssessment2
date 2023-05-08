bbeb9c52a709d1472ac93ea1b383dda859d513a6
Matt-Bruce111 <Matt.bruce111@gmail.com>
2023-05-08 12:04:19 +1000

    Added Table Attributes

---
+++ MVCAssessment2/Models/Universities.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCAssessment2.Models
+{
+    public class Universities
+    {
+        [Key]
+        public int uniID { get; set; }
+
+        public string universityName { get; set; }
+
+        public int uniRank { get; set; }
+    }
+}
